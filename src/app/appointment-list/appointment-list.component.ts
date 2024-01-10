954c7d35f9eed49ff0a90db7cc4acd0a43492c64
Senali-perera <senaliperera1996@gmail.com>
2024-01-10 23:41:18 +0200

    Load the appointments from the local storage using ngOnInit

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Appointment} from "../models/appointment";
 import { FormsModule } from '@angular/forms';
 import {DatePipe, NgForOf} from "@angular/common";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-appointment-list',
@@ -14,11 +15,16 @@ import {DatePipe, NgForOf} from "@angular/common";
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   newAppointmentTitle : string = "";
   newAppointmentDate : Date = new Date();
 
   appointments: Appointment[] = []
+
+  ngOnInit(): void{
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
